c2f53b64693b195a380fc4bff1774620abd3eca1
YOSHIE MIZOGUCHI <thisisamamail@gmail.com>
2016-12-06 20:13:18 +0900

    レビューによる変更：バリデーション追加

--- Model/User.php
+++ Model/User.php
@@ -15,6 +15,62 @@ class User extends AppModel {
      * @var string
      */
     public $displayField = 'username';
+    public $validate = [
+        'username' => [
+            'required' => [
+                'rule' => 'notBlank',
+                'message' => 'ユーザーネームを入力してください'
+            ],
+        ],
+        'email' => [
+            'required' => [
+                'rule' => 'notBlank',
+                'message' => 'メールアドレスを入力してください'
+            ],
+            'validEmail' => [
+                'rule' => 'email',
+                'message' => '正しいメールアドレスを入力してください'
+            ],
+            'emailExists' => [
+                'rule' => ['isUnique', 'email'],
+                'message' => '入力されたメールアドレスは既に登録されています'
+            ],
+        ],
+        'password' => [
+            'required' => [
+                'rule' => 'notBlank',
+                'message' => 'パスワードを入力してください'
+            ],
+            // バリデーションにメソッドを指定
+            'match' => [
+                'rule' => 'passwordConfirm',
+                'message' => 'パスワードが一致していません'
+            ],
+        ],
+        'password_confirm' => [
+            'required' => [
+                'rule' => 'notBlank',
+                'message' => 'パスワード(確認)を入力してください'
+            ],
+        ],
+        'role' => [
+            'required' => [
+                'rule' => 'notBlank',
+                'message' => '権限を入力してください'
+            ],
+        ],
+    ];
+
+    // カスタムバリデーションメソッド
+    public function passwordConfirm($check) {
+
+        // $check は ['password' => '入力された値']
+        if ($check['password'] === $this->data['User']['password_confirm']) {
+            return true;
+        }
+
+        return false;
+    }
 
     public function beforeSave($options = []) {
         // パスワードをハッシュ化
